2a442d426c784531ca16ee112ff71f62e171b78a
sauravadiyodi <saurav.adiyodi@gmail.com>
2023-11-13 12:41:13 +0530

    New player UI

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,55 +1,67 @@
 // import logo from './logo.svg';41
 import { useEffect, useState } from 'react';
-import '../styles/Home.css';
-import logo from '../assets/clapperboard.png';
+import '../styles/Player.css';
+// import logo from '../assets/clapperboard.png';
 
 function Player() {
   const server = 'https://vidstream-server-o9cb.onrender.com';
-  const [results, setResults] = useState([]);
-  const [query, setQuery] = useState('');
+  // let activeServer = '1';
+  const [result, setResult] = useState({ src: '', splash: '', poster: '', title: '', year: '', desc: '' });
+  const [activeServer, setActiveServer] = useState(1);
+  // const [loading, setLoading] = useState(true);
+
+  // useEffect(()=>{
+
+
+  // },[])
 
   useEffect(() => {
+    let urlParams = new URLSearchParams(window.location.search);
+    let movieId = urlParams.get('source');
+    console.log(movieId)
+
     async function fetchSearch() {
-      if (query !== '') {
-        let movieData = await fetch(`${server}/search?q=${encodeURIComponent(query)}`, { method: "GET" });
-        let movieObj = await movieData.json();
-        setResults(movieObj.body);
+      if (movieId !== '') {
+        try {
+          let movieLinkData = await fetch(`${server}/movie/stream/${encodeURIComponent(movieId)}?server=${activeServer}`, { method: "GET" });
+          let movieLink = (await movieLinkData.json()).link;
+          console.log(movieLink)
+
+          let movieData = await (await fetch(`${server}/movie/details/${encodeURIComponent(movieId)}`, { method: "GET" })).json();
+          console.log(movieData)
+          // console.log(movieObj)
+          setResult({ src: 'movieLink', splash: movieData.backdrop, poster: movieData.poster, title: movieData.title, year: movieData.year, desc: movieData.description });
+        }
+        catch (err) {
+          console.log(err);
+        }
       }
       else {
-        setResults([]);
+        setResult({ src: '', splash: '', poster: '', title: '', year: '', desc: '' });
+        window.location.href = '/';
       }
     }
     fetchSearch();
-  }, [query]);
+  }, [activeServer]);
 
   return (
-    <div className="landing">
-      <div className="logo_container">
-        <img alt="vidstream_logo" className="logo" src={logo} />
-        <p style={{ margin: "0px" }}>Vidstream</p>
+    <div className="page-container">
+      <div className="splash" style={{ background: `linear-gradient(transparent, black), url(${result.splash}) no-repeat` }}></div>
+      <div className="media-block">
+        <iframe title="MoviePlayer" className="player-window" src={result.src} frameborder="0" sandbox> </iframe>
+        <div className='server-selector'>
+          <div className='server-button' onClick={() => setActiveServer(1)}>Server 1</div>
+          <div className='server-button' onClick={() => setActiveServer(2)}>Server 2</div>
+        </div>
+        <div className="info-container">
+          <img className="poster" alt="Movie Poster" src={result.poster} />
+          <div className="media-info">
+            <div className="media-title">{result.title}</div>
+            <div className="media-year">{result.year}</div>
+            <div className="media-desc">{result.desc}</div>
+          </div>
+        </div>
       </div>
-      <input type="text" placeholder="Search for a movie..." className="search-box" onChange={(event) => { setQuery(event.target.value) }} />
-      {
-        results.length > 0 &&
-        <>
-          <div className="search-results">
-            {results.map((item) => {
-              return (<>
-                <div className="result" id={item.id}>
-                  <img className="res-thumb" alt={item.title} src={item.poster} />
-                  <div className="res-info">
-                    <div className="res-title">{item.title}</div>
-                    <div className="res-year">{item.year}</div>
-                    <div className="res-desc">{item.overview}</div>
-                  </div>
-                </div>
-                <div className="horizontal-divider"></div>
-              </>)
-
-            })}
-          </div >
-        </>
-      }
     </div >
   );
 }
